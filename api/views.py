290152e98a8c291d796846d46da81f8427c88797
ShehzadDev <shehzadbhutta@outlook.com>
2024-09-26 06:07:39 -0400

    Queryset

--- api/views.py
+++ api/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+# from .models import Project
 
-# Create your views here.
+# # Create your views here.
+
+
+# def projects(request):
+#     projects = Project.objects.all()
+#     return render(request, "project.html", {"projects": projects})
+
+
+from django.shortcuts import render
+from .models import Project, User
+
+
+def index(request):
+    return render(request, "base.html")
+
+
+def project(request):
+    projects = Project.objects.all()
+    return render(request, "project.html", {"projects": projects})
+
+
+def users(request):
+    users = User.objects.all()
+    return render(request, "users.html", {"users": users})
